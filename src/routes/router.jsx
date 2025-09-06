e5d2255546315c80234d41fdc193c181d77feb52
Mezbahul-241-15-929 <islam241-15-929@diu.edu.bd>
2025-09-07 01:24:20 +0600

    first commit

---
+++ src/routes/router.jsx
@@ -0,0 +1,68 @@
+import { createBrowserRouter } from "react-router";
+import HomeLayout from "../layouts/HomeLayout";
+import CategoryNews from "../pages/CategoryNews";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import AuthLayout from "../layouts/AuthLayout";
+import NewsDetails from "../pages/NewsDetails";
+import PrivateRoute from "../provider/PrivateRoute";
+import Loading from "../pages/Loading";
+import PageNotfound from "../pages/PageNotfound";
+import AboutMe from "../pages/Aboutme";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout></HomeLayout>,
+    children: [
+      {
+        path: "",
+        element: <Home></Home>,
+      },
+
+      {
+        path: "/category/:id",
+        element: <CategoryNews></CategoryNews>,
+        loader: () => fetch("/news.json"),
+        hydrateFallbackElement: <Loading></Loading>,
+      },
+      
+
+    ],
+  },
+  {
+    path: "/auth",
+    element: <AuthLayout></AuthLayout>,
+    children: [
+      {
+        path: "/auth/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/auth/register",
+        element: <Register></Register>,
+      },
+    ],
+  },
+  {
+    path: "/news-details/:id",
+    element: (
+      <PrivateRoute>
+        <NewsDetails></NewsDetails>
+      </PrivateRoute>
+    ),
+    loader: () => fetch("/news.json"),
+    hydrateFallbackElement: <Loading></Loading>,
+  },
+  {
+    path: "/*",
+    element: <PageNotfound></PageNotfound>,
+  },
+  {
+    path: "/about",
+    element: <AboutMe></AboutMe>,
+  },
+]);
+
+export default router;
